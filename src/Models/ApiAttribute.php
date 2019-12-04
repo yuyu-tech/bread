c69edddcb06b7b682e966f87ee210ca00783573d
Bhushan Mer <bhushanmer@gmail.com>
2019-12-04 21:47:48 +0530

    DB connction for this package was modified to 'bread'

--- src/Models/ApiAttribute.php
+++ src/Models/ApiAttribute.php
@@ -1,10 +1,15 @@
 <?php
 
-namespace App\Models;
+namespace Yuyu\BREAD\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
 class ApiAttribute extends Model
 {
-    //
+    /**
+     * The connection name for the model.
+     *
+     * @var string
+     */
+    protected $connection = 'bread';
 }
--- src/Models/ApiRole.php
+++ src/Models/ApiRole.php
@@ -1,10 +1,15 @@
 <?php
 
-namespace App\Models;
+namespace Yuyu\BREAD\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
 class ApiRole extends Model
 {
-    //
+    /**
+     * The connection name for the model.
+     *
+     * @var string
+     */
+    protected $connection = 'bread';
 }
