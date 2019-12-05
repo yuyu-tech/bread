8eb8597c348b94c6ccfc8a5a35d88f3e56fbdc66
Bhushan Mer <bhushanmer@gmail.com>
2019-12-05 19:53:03 +0530

    Relationship added: Relationship hasManyThrough relation with Attributes

--- src/Models/Relationship.php
+++ src/Models/Relationship.php
@@ -12,4 +12,21 @@ class Relationship extends Model
      * @var string
      */
     protected $connection = 'bread';
+
+    /**
+     * Get the relationship table that owns the relationship.
+     */
+    public function attributes()
+    {
+        $arrRoleIds = session('api_role_ids');
+        $arrRoleIds = is_array($arrRoleIds) ? $arrRoleIds : explode(',', $arrRoleIds);
+        
+        return $this->hasManyThrough('App\Models\Bread\Attribute', 'App\Models\Bread\ApiAttribute', 'relatioship_id', 'id', 'id', 'attribute_id')
+            ->selectRaw('attributes.*, SUM(api_attributes.search) as search, SUM(api_attributes.relation_filter) as relation_filter, SUM(api_attributes.listing) as listing')
+            ->join('api_role_api_attribute', 'api_role_api_attribute.api_attribute_id', '=', 'api_attributes.id')
+            ->whereIn('api_role_api_attribute.api_role_id', $arrRoleIds)
+            ->where('attributes.status', 1)
+            ->where('api_attributes.status', 1)
+            ->groupBy('attributes.id');
+    }
 }
